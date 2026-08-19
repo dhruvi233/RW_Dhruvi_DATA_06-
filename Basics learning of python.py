0bac58f3754fdcc5b754be214737aacf4f7b205e
dhruvi233 <dasdhruvi233@gmail.com>
2026-08-19 20:09:47 +0530

    Add files via upload

---
+++ Basics learning of python.py
@@ -0,0 +1,34 @@
+print("Hello World!!")
+Name= input("What is your name")
+Age= input("What is your age")
+print("Hello", Name, "you are age", Age, "year old")
+
+
+a=3
+b=7
+sum= a+b
+print("sum:",sum)
+
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+sum= a+b
+print("sum", sum)
+
+
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Expontiations:", a**b)
+
+Pet=input("What pet do you have")
+If pet == "Dog":
+print("bhow bhow! I want a special treatment")
+Elif pet=="cat":
+print("Meow meow! I am special")
+Elif pet=="fish":
+print("Blub blub, I am swimming in style")
+Else:
+print("What a unique pet you have")
