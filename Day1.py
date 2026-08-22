5f77b53105dcccc80fb7644bb998ca215ab67150
dhruvi233 <dasdhruvi233@gmail.com>
2026-08-22 23:22:06 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,35 @@
+print("Hello World!!")
+Name= input("What is your Name?")
+Age= int(input("What is your age?"))
+print("Name",Name)
+print("Age", Age)
+
+a=5
+b=7
+print("sum", a+b)
+
+a=10
+b=5
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a/b)
+print("Expontation", a**b)
+
+pet= input("What pet do you have (dog/cat/fish):")
+if pet=="dog":
+    print("Bhaw bhaw! I want a special treatment")
+
+elif pet== "cat":
+    print("Meow meow! I am special")
+
+elif pet== "fish":
+    print("Blub blub, I am swimming")
+
+else:
+ print("That's a unique pet")
+
+a= input("Enter first number:")
+b= input("Enter second number:")
+sum= int(a) + int(b)
+print ("sum", sum)
---
+++ Python operators.py
@@ -0,0 +1,117 @@
+#Arithemetic operators
+
+
+
+a=2
+b=8
+print(a+b)
+print(a-b)
+print (a*b)
+print (a/b)  
+print(a//b)        
+print (a**b)  
+print (a%b) 
+
+#Assignment operator
+
+a=10
+a+=5
+print(a)
+
+a=15
+a+=10
+print(a)
+
+b=20
+b-=10
+print(b)
+
+c=5
+c*=5
+print(c)
+
+d=8
+d/=2
+print(d)
+
+e=4
+e//=1
+print(e)
+
+x=5
+x**=5
+print(x)
+
+z=10
+z%=3
+print(z)
+
+
+#Comparison operator
+
+a=100
+b=94
+print(a>b)
+print (a<b)
+print(a==b)
+print(a!=b)
+print (a>=b)
+print (a<=b)
+
+
+
+
+
+#Logical operator
+
+a=10
+b=20
+print(a>5 and b>15)
+print (a<15 and b<15)
+print (a > 15 or b > 15)
+
+
+a=20
+print(not(a > 10))
+print (not(a > 30))
+
+
+#Membership operator
+
+fruits = ["apple", "banana", "mango"]
+print("banana" in fruits)
+print ("orange" in fruits)
+print("apple" not in fruits)
+
+
+#Identity operator
+
+
+
+a=[1,2,3]
+b=[1,2,3]
+print (a == b)
+print (a is b)
+
+
+a=[10,20]
+b=a
+print (a is b)
+print (a is not b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
