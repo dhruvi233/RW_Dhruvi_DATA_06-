ca47c70f32dde5db4f0a044484d617ae61dd40be
dhruvi233 <dasdhruvi233@gmail.com>
2026-08-23 18:05:37 +0530

    Add files via upload

---
+++ ProjectTask_1.py
@@ -0,0 +1,70 @@
+#Fundamental Booster
+# Personal Data Collector
+
+print("Welcome to the Personal Data Collector!")
+print ("Let's collect some information about you")
+print ("Let's get started!")
+
+
+
+Name = input("What is your Name?")
+Age = int(input("How old are you?"))
+Height = float(input("What is your height in meters?"))
+Favourite_Number = int(input("What is your favourite number?"))
+print("Name:",Name)
+print("Age:", Age)
+print ("Height",Height)
+print ("Favourite_Number:",Favourite_Number)
+
+
+a = 2026
+b = Age
+birth_year = a - b
+print (birth_year)
+
+print("Name:",Name)
+print("Data Type:", type(Name))
+print("Memory Address:", id(Name))
+
+
+print("Age:", Age)
+print ("Data Type:",type(Age))
+print ("Memory Address:",id(Age))
+
+
+print ("Height",Height)
+print ("Data Type:",type(Height))
+print("Memory Address:",id(Height))
+
+
+
+
+print ("Favourite_Number",Favourite_Number)
+print("Data Type:",type(Favourite_Number))
+print ("Memory Address:",id(Favourite_Number))
+
+
+print("Thank You user!")
+print("We have collected all the Data needed to us!")
+print ("Here is the information we have collected:")
+print("Name:",Name)
+print("Age:", Age)
+print ("Height:", Height)
+print ("Favourite_Number:",Favourite_Number)
+print ("Your approximately birth_year is", birth_year)
+print ("Thank You for providing your information.")
+print ("Keep learning and exploring Python!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
